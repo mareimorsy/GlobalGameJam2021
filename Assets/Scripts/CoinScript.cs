421202f855a24aa9917342dd1da7e275a975bd55
Marei Morsy <mareimorsy@yahoo.com>
2021-01-22 00:06:24 +0200

    first commit

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    private LevelManager gameLevelManager;
+    public int coinValue = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        gameLevelManager = FindObjectOfType<LevelManager> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+
+        if(other.tag == "Player"){
+            gameLevelManager.AddCoins(coinValue);
+            Debug.Log("Score = " + gameLevelManager.score);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    public float respawnDelay;
+    // PlayerController Script
+    public PlayerController gamePlayer;
+
+    public int score = 0;
+
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamePlayer = FindObjectOfType<PlayerController> ();
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Respawn(){
+        StartCoroutine("RespawnCoroutine");
+    }
+
+    public IEnumerator RespawnCoroutine(){
+        // gemeObject refers to the object which the script is attached to which is the player
+        gamePlayer.gameObject.SetActive(false);
+        yield return new WaitForSeconds (respawnDelay);
+        gamePlayer.transform.position = gamePlayer.respawnPoint; 
+        gamePlayer.gameObject.SetActive(true);
+    }
+
+    public void AddCoins( int numOfCoins){
+        score += numOfCoins;
+        scoreText.text = "Score: " + score;
+    }
+}
