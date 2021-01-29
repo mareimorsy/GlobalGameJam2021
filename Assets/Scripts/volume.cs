b9a47d9671ca3dd72bd1be04284bf1dd08d3ac22
loai-osama <77856791+loai-osama@users.noreply.github.com>
2021-01-29 21:41:41 +0200

    volume
    Co-Authored-By: Marei Morsy <11376730+mareimorsy@users.noreply.github.com>

---
+++ Assets/Scripts/volume.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class volume : MonoBehaviour
+{
+    public AudioMixer audiomixer;
+    public void SetVolume(float volume)
+    {
+       audiomixer.SetFloat("volume",volume);
+    }
+}
